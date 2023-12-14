8bd9c5509d2676c245deb0f3f375a5720f1190fa
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-12-14 15:10:46 +0300

    Debounce input

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { sendMessage } from 'webext-bridge/options'
+import { debounce } from 'lodash-es'
 import { userName } from '~/logic/storage'
 import ItemList from '~/components/ItemList.vue'
 import { store } from '~/logic/store'
@@ -10,20 +11,25 @@ const query = ref('')
 watch(
   userName,
   (newValue) => {
-    status.value = !newValue ? 'username is required' : 'ok'
+    status.value = !newValue ? 'username is required' : ''
   },
   { immediate: true },
 )
 
+const update = debounce((e: Event) => {
+  query.value = (e.target as HTMLInputElement)?.value
+}, 100)
+
 async function fetchPosts() {
-  const res = await sendMessage('fetch-saved', { username: userName.value }, 'background')
-  store.isFetching = res.isFetching
+  if (userName.value) {
+    const res = await sendMessage('fetch-saved', { username: userName.value }, 'background')
+    store.isFetching = res.isFetching
+  }
 }
 </script>
 
 <template>
   <main class="px-4 py-10 text-center text-gray-700">
-
     <input v-model="userName" class="border border-gray-400 rounded px-2 py-1 mt-2" />
 
     <div class="mt-4">
@@ -34,7 +40,7 @@ async function fetchPosts() {
 
     <div class="mt-4">
       <b class="mr-2">Search:</b>
-      <input v-model="query" class="border border-gray-400 rounded px-2 py-1 mt-2" />
+      <input class="border border-gray-400 rounded px-2 py-1 mt-2" :value="query" @input="update" />
     </div>
 
     <div class="mt-4">
