88b58d0ce2b81962e4b94f4923a49d48ea2fef70
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-02-12 00:42:31 +0300

    Update primevue

--- src/styles/presets/wind/index.js
+++ src/styles/presets/wind/index.js
@@ -1,11 +1,34 @@
-import global from './global.js';
+import accordion from './accordion';
 import autocomplete from './autocomplete';
+import avatar from './avatar';
+import avatargroup from './avatargroup';
+import badge from './badge';
+import badgedirective from './badgedirective';
+import blockui from './blockui';
+import breadcrumb from './breadcrumb';
+import button from './button';
 import calendar from './calendar';
+import card from './card';
+import carousel from './carousel';
 import cascadeselect from './cascadeselect';
 import checkbox from './checkbox';
+import chip from './chip';
 import chips from './chips';
 import colorpicker from './colorpicker';
+import confirmpopup from './confirmpopup';
+import contextmenu from './contextmenu';
+import datatable from './datatable';
+import dataview from './dataview';
+import dataviewlayoutoptions from './dataviewlayoutoptions';
+import dialog from './dialog';
+import divider from './divider';
+import dock from './dock';
 import dropdown from './dropdown';
+import fieldset from './fieldset';
+import galleria from './galleria';
+import global from './global';
+import image from './image';
+import inlinemessage from './inlinemessage';
 import inputgroup from './inputgroup';
 import inputgroupaddon from './inputgroupaddon';
 import inputmask from './inputmask';
@@ -14,136 +37,147 @@ import inputswitch from './inputswitch';
 import inputtext from './inputtext';
 import knob from './knob';
 import listbox from './listbox';
-import multiselect from './multiselect';
-import password from './password';
-import radiobutton from './radiobutton';
-import rating from './rating';
-import selectbutton from './selectbutton';
-import slider from './slider';
-import textarea from './textarea';
-import togglebutton from './togglebutton';
-import treeselect from './treeselect';
-import tristatecheckbox from './tristatecheckbox';
-import button from './button';
-import splitbutton from './splitbutton';
-import datatable from './datatable';
-import dataview from './dataview';
-import orderlist from './orderlist';
-import organizationchart from './organizationchart';
-import paginator from './paginator';
-import picklist from './picklist';
-import tree from './tree';
-import treetable from './treetable';
-import message from './message';
-import inlinemessage from './inlinemessage';
-import toast from './toast';
-import breadcrumb from './breadcrumb';
-import contextmenu from './contextmenu';
-import dock from './dock';
+import megamenu from './megamenu';
 import menu from './menu';
 import menubar from './menubar';
-import megamenu from './megamenu';
+import message from './message';
+import multiselect from './multiselect';
+import orderlist from './orderlist';
+import organizationchart from './organizationchart';
+import overlaypanel from './overlaypanel';
+import paginator from './paginator';
+import panel from './panel';
 import panelmenu from './panelmenu';
+import password from './password';
+import picklist from './picklist';
+import progressbar from './progressbar';
+import radiobutton from './radiobutton';
+import rating from './rating';
+import ripple from './ripple';
+import scrollpanel from './scrollpanel';
+import scrolltop from './scrolltop';
+import selectbutton from './selectbutton';
+import sidebar from './sidebar';
+import skeleton from './skeleton';
+import slider from './slider';
+import speeddial from './speeddial';
+import splitbutton from './splitbutton';
 import steps from './steps';
 import tabmenu from './tabmenu';
-import tieredmenu from './tieredmenu';
-import accordion from './accordion';
-import card from './card';
-import divider from './divider';
-import fieldset from './fieldset';
-import panel from './panel';
-import scrollpanel from './scrollpanel';
 import tabview from './tabview';
-import toolbar from './toolbar';
-import confirmpopup from './confirmpopup';
-import dialog from './dialog';
-import overlaypanel from './overlaypanel';
-import sidebar from './sidebar';
-import tooltip from './tooltip';
-import avatar from './avatar';
-import badge from './badge';
-import badgedirective from './badgedirective';
-import chip from './chip';
-import scrolltop from './scrolltop';
-import skeleton from './skeleton';
-import progressbar from './progressbar';
-import ripple from './ripple';
 import tag from './tag';
 import terminal from './terminal';
+import textarea from './textarea';
+import tieredmenu from './tieredmenu';
+import timeline from './timeline';
+import toast from './toast';
+import togglebutton from './togglebutton';
+import toolbar from './toolbar';
+import tooltip from './tooltip';
+import tree from './tree';
+import treeselect from './treeselect';
+import treetable from './treetable';
+import tristatecheckbox from './tristatecheckbox';
 
 export default {
     global,
     directives: {
-        tooltip,
-        badgedirective,
-        ripple
+        badge: badgedirective,
+        ripple,
+        tooltip
     },
+
+    //forms
     autocomplete,
-    calendar,
-    cascadeselect,
-    checkbox,
-    chips,
-    colorpicker,
     dropdown,
+    inputnumber,
+    inputtext,
+    calendar,
+    checkbox,
+    radiobutton,
+    inputswitch,
+    selectbutton,
+    slider,
+    chips,
+    rating,
+    multiselect,
+    togglebutton,
+    cascadeselect,
+    listbox,
+    colorpicker,
     inputgroup,
     inputgroupaddon,
     inputmask,
-    inputnumber,
-    inputswitch,
-    inputtext,
     knob,
-    listbox,
-    multiselect,
-    password,
-    radiobutton,
-    rating,
-    selectbutton,
-    slider,
-    textarea,
-    togglebutton,
     treeselect,
     tristatecheckbox,
+    textarea,
+    password,
+
+    //buttons
     button,
     splitbutton,
-    datatable,
-    dataview,
-    orderlist,
-    organizationchart,
+    speeddial,
+
+    //data
     paginator,
-    picklist,
+    datatable,
     tree,
+    dataview,
+    dataviewlayoutoptions,
+    organizationchart,
+    orderlist,
+    picklist,
     treetable,
-    message,
-    inlinemessage,
-    toast,
-    breadcrumb,
+    timeline,
+
+    //panels
+    accordion,
+    panel,
+    fieldset,
+    card,
+    tabview,
+    divider,
+    toolbar,
+    scrollpanel,
+
+    //menu
     contextmenu,
-    dock,
     menu,
     menubar,
-    megamenu,
-    panelmenu,
     steps,
-    tabmenu,
     tieredmenu,
-    accordion,
-    card,
-    divider,
-    fieldset,
-    panel,
-    scrollpanel,
-    tabview,
-    toolbar,
-    confirmpopup,
+    breadcrumb,
+    panelmenu,
+    megamenu,
+    dock,
+    tabmenu,
+
+    //overlays
     dialog,
     overlaypanel,
     sidebar,
-    avatar,
+    confirmpopup,
+
+    //messages
+    message,
+    inlinemessage,
+    toast,
+
+    //media
+    carousel,
+    galleria,
+    image,
+
+    //misc
     badge,
-    chip,
-    scrolltop,
-    skeleton,
-    progressbar,
+    avatar,
+    avatargroup,
     tag,
-    terminal
-}
+    chip,
+    progressbar,
+    skeleton,
+    scrolltop,
+    terminal,
+    blockui
+};
--- src/styles/presets/wind/inputswitch/index.js
+++ src/styles/presets/wind/inputswitch/index.js
@@ -1,5 +1,5 @@
 export default {
-    root: ({ props, state }) => ({
+    root: ({ props }) => ({
         class: [
             // Alignments
             'inline-flex relative',
@@ -12,7 +12,6 @@ export default {
             'h-5 w-9',
 
             // States
-            { 'outline-none outline-offset-0 ring-2 ring-primary-500 ring-offset-2 ring-offset-white dark:ring-offset-gray-900 dark:ring-primary-400': state.focused },
             {
                 'opacity-60 select-none pointer-events-none cursor-default': props.disabled
             }
@@ -34,17 +33,19 @@ export default {
             'before:duration-200 before:transition before:ease-in-out',
             'before:bg-surface-0 before:dark:bg-surface-900',
             'before:shadow',
-            { 'before:transform before:translate-x-4': props.modelValue },
+            { 'before:transform before:translate-x-4': props.modelValue == props.trueValue },
 
             // Colors
             'border-2 border-transparent',
             {
-                'bg-surface-200 dark:bg-surface-700': !props.modelValue,
-                'bg-primary-500 dark:bg-primary-400': props.modelValue
+                'bg-surface-200 dark:bg-surface-700': !(props.modelValue == props.trueValue),
+                'bg-primary-500 dark:bg-primary-400': props.modelValue == props.trueValue
             },
 
             // States
-            { 'hover:bg-surface-300 hover:dark:bg-surface-600 ': !props.modelValue },
+            { 'peer-hover:bg-surface-300 dark:peer-hover:bg-surface-600 ': !(props.modelValue == props.trueValue) && !props.disabled },
+            { 'peer-hover:bg-primary-600 dark:peer-hover:bg-surface-300 ': (props.modelValue == props.trueValue) && !props.disabled },
+            'peer-focus-visible:ring-2 peer-focus-visible:ring-primary-500 dark:peer-focus-visible:ring-primary-400',
 
             // Transition
             'transition-colors duration-200',
@@ -52,5 +53,36 @@ export default {
             // Misc
             'cursor-pointer'
         ]
-    })
+    }),
+    input: {
+        class: [
+            'peer',
+
+            // Size
+            'w-full ',
+            'h-full',
+
+            // Position
+            'absolute',
+            'top-0 left-0',
+            'z-10',
+
+            // Spacing
+            'p-0',
+            'm-0',
+
+            // Shape
+            'rounded',
+            'border',
+
+            // Shape
+            'opacity-0',
+            'rounded-md',
+            'outline-none',
+            'border-2 border-surface-300 dark:border-surface-700',
+
+            // Misc
+            'appareance-none'
+        ]
+    }
 };
