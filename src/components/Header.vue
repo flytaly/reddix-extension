fec43f1d3faa5be238999a5469f4c538b1946cc6
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-02-22 10:15:27 +0300

    Override header height

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -35,7 +35,7 @@ const accSidebarOn = ref(false)
   <header>
     <Menubar
       :model="items"
-      :pt="{ root: 'min-h-[1.5rem] bg-surface-50 dark:bg-surface-950', menu: '!py-0.5' }"
+      :pt="{ root: '!min-h-[2.5rem] bg-surface-50 dark:bg-surface-950', menu: '!py-0.5' }"
       :pt-options="{ mergeProps: true }"
     >
       <template #start>
