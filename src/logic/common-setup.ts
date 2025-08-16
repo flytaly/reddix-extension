6ca81b9e567a1f90ecfee25d317e808ffa2e26a7
Vitaly Yerofeyevsky <flytaly@gmail.com>
2025-08-17 01:39:49 +0300

    ref: remove Primevue

--- src/logic/common-setup.ts
+++ src/logic/common-setup.ts
@@ -1,15 +1,9 @@
 import type { App } from 'vue'
-import PrimeVue from 'primevue/config'
-import ConfirmationService from 'primevue/confirmationservice'
-import Toast from 'primevue/toast'
-import ToastService from 'primevue/toastservice'
-import Tooltip from 'primevue/tooltip'
 import { DynamicScroller, DynamicScrollerItem } from 'vue-virtual-scroller'
 import pkg from '~/../package.json'
-
 import { setupTheme } from '~/logic/theme'
+
 import { router } from '~/options/routes'
-import PrimePreset from '~/styles/presets/wind'
 import 'vue-sonner/style.css'
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 
@@ -25,22 +19,8 @@ export function setupApp(app: App, appContext: { context?: string } = {}) {
   // Provide access to `app` in script setup with `const app = inject('app')`
   app.provide('app', app.config.globalProperties.$app)
 
-  // Here you can install additional plugins for all contexts: popup, options page and content-script.
-  // example: app.use(i18n)
-  // example excluding content-script context: if (context !== 'content-script') app.use(i18n)
-  app.use(PrimeVue, {
-    unstyled: true,
-    pt: PrimePreset,
-  })
-
-  app.use(ConfirmationService)
   app.use(router)
-  app.use(ToastService)
-  app.directive('tooltip', Tooltip)
 
   app.component('DynamicScroller', DynamicScroller)
   app.component('DynamicScrollerItem', DynamicScrollerItem)
-
-  // for some reason it won't work with autoimport
-  app.component('Toast', Toast)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import Vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Icons from 'unplugin-icons/vite'
-import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
@@ -49,7 +48,6 @@ export const sharedConfig: UserConfig = {
         IconsResolver({
           prefix: '',
         }),
-        PrimeVueResolver(),
       ],
     }),
 
